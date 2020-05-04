8141e34e555770d97dd1436b117975bc61637d53
Jared Middleton <jaredmiddleton3.14@gmail.com>
2020-05-04 15:52:47 -0700

    Fix connection to mongo with express-session and connect-mongo

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(
     saveUninitialized: true,
     secret: "aaabbbccc",
     store: new MongoStore({
-      url: MONGO_URI,
+      mongooseConnection: mongoose.connection,
       autoReconnect: true,
     }),
   })
